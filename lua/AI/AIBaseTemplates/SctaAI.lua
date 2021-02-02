7873ad0ad43b7688044d0f1abcd1d662b987d441
Aaron Warner <aaronpaulwarner@gmail.com>
2021-02-02 14:28:04 +1300

    initial

---
+++ lua/AI/AIBaseTemplates/SctaAI.lua
@@ -0,0 +1,35 @@
+--[[
+    File    :   /lua/AI/AIBaseTemplates/MicroAI.lua
+    Author  :   SoftNoob
+    Summary :
+        Lists AIs to be included into the lobby, see /lua/AI/CustomAIs_v2/SorianAI.lua for another example.
+        Loaded in by /lua/ui/lobby/aitypes.lua, this loads all lua files in /lua/AI/CustomAIs_v2/
+]]
+
+BaseBuilderTemplate {
+    BaseTemplateName = 'MicroAI',
+    Builders = {
+        -- List all our builder grous here
+        'MicroAICommanderBuilder',
+        'MicroAIEngineerBuilder',
+        'MicroAILandBuilder',
+        'MicroAIAirBuilder',
+        'MicroAIPlatoonBuilder',
+    },
+    NonCheatBuilders = {
+        -- Specify builders that are _only_ used by non-cheating AI (e.g. scouting)
+    },
+    BaseSettings = { },
+    ExpansionFunction = function(aiBrain, location, markerType)
+        -- This is used if you want to make stuff outside of the starting location.
+        return 0
+    end,
+    
+    FirstBaseFunction = function(aiBrain)
+        local per = ScenarioInfo.ArmySetup[aiBrain.Name].AIPersonality
+        if per == 'microai' or per == 'microaicheat' then
+            return 1000, 'MicroAI'
+        end
+        return -1
+    end,
+}
---
+++ mod_info.lua
@@ -0,0 +1,20 @@
+name = "SCTAAI"
+uid = "15bbbd89-26fc-475a-b6ae-49813eaf6498"
+version = 2
+copyright = "FreeWare"
+description = "SCTA AI implementation."
+author = "relentless"
+url = "https://github.com/HardlySoftly/MicroAI"
+icon = "/mods/SCTAAI/SCTAAI.jpg"
+selectable = true
+enabled = true
+exclusive = false
+ui_only = false
+requires = { }
+requiresNames = { }
+conflicts = { }
+before = { }
+after = { 
+    "SCTAFIX-AB75-22EA-A7CA-D6DA9300g8a9",
+    "SCTABALANCE-AB75-225f-5i47-b27h1c6636a9"
+}
